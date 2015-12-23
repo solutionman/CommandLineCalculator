7a199529413f37dbe40e487a1360ea9f16e26345
lom <diskedge@yandex.ru>
2015-12-23 13:00:01 +0700

    continue practice with second version

--- src/CommandLineCalculator2.java
+++ src/CommandLineCalculator2.java
@@ -7,15 +7,40 @@ public class CommandLineCalculator2 {
 
         String inputNumbers;
 
-
+        String plus = "+";
+        String minus = "-";
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the number");
+        System.out.println("Enter the number and press \"Enter\"");
+
+        double x = scan.nextDouble();
+
+        System.out.println("You entered " + x);
+        System.out.println("Now enter the operation you need to perform");
 
 
-        
+        String sign = scan.next();
+
+        System.out.println("You entered " + sign);
+
+        System.out.println("Now enter another number and press \"Enter\"");
+
+        double y = scan.nextDouble();
+        System.out.println("Your second number is " + y);
+
+
+        if (sign.equals(plus)) {
+            double z = x + y;
+            System.out.println("The sum of your numbers is " + z );
+        }
+        else {
+            System.out.println("Something goes wrong...");
+        }
 
 
     }
 }
+
+
+
