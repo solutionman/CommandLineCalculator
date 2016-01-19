49937ee65ed006fdb86741987da188408e1c8927
lom <diskedge@yandex.ru>
2016-01-19 19:20:11 +0700

    Finally version 4 is working, only with 2 numbers

--- src/CommandLineCalculator4.java
+++ src/CommandLineCalculator4.java
@@ -1,43 +1,51 @@
 // Here we will compute the whole line
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class CommandLineCalculator4 {
     public static void main(String args[]) {
 
-      String one = "1", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9", zero = "0";
-        String plus = "+", minus = "-", divide = "/", multiply = "*";
-        String num1 = "";
-        String num2 = "";
-        String sign = "";
+      String plus = "+", minus = "-", divide = "/", multiply = "*";
+      String num1 = "";
+      String num2 = "";
+      String sign = "";
 
-        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter first number, then + then second number and press \"Enter\"");
 
-        String task = scan.next();
+      Scanner scan = new Scanner(System.in);
 
-        String delims = "[+]";
+      System.out.println("Enter first number, then + then second number and press \"Enter\"");
 
-        String[] splitted = task.split(delims);
+      String task = scan.next();
 
-        //System.out.println(Arrays.toString(splitted));
-
-        int size = splitted.length;
-        for (int i = 0; i < size; i++) {
-            num1 = splitted[0];
-            num2 = splitted[1];
-        }
+        num1 = "" + task.charAt(0);
+        sign = "" + task.charAt(1);
+        num2 = "" + task.charAt(2);
 
         Double doublenum1 = Double.parseDouble(num1);
         Double doublenum2 = Double.parseDouble(num2);
 
-        Double result = doublenum1 + doublenum2;
-        System.out.println("Result of your expressin is: " + result);
+        if (sign.equals(plus)) {
+            double z = doublenum1 + doublenum2;
+            System.out.println(doublenum1 + " + " + doublenum2 + " = " + z);
+        } else if (sign.equals(minus)) {
+            double z = doublenum1 - doublenum2;
+            System.out.println(doublenum1 + " - " + doublenum2 + " = " + z);
+        } else if (sign.equals(divide)) {
+            double z = doublenum1 / doublenum2;
+            System.out.println(doublenum1 + " / " + doublenum2 + " = " + z);
+        } else if (sign.equals(multiply)) {
+            double z = doublenum1 * doublenum2;
+            System.out.println(doublenum1 + " * " + doublenum2 + " = " + z);
+        } else {
+            System.out.println("It looks, like you choose wrong sign...");
+
+        }
+
 
     }
 }
+
 
 
 
