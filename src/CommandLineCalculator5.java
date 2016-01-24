d3b0e463bdd7829dda884a54e13d877c6207d794
dima <dima@localhost.localdomain>
2016-01-24 18:40:29 +0600

    I did it ) now we can read the sign

--- src/CommandLineCalculator5.java
+++ src/CommandLineCalculator5.java
@@ -6,6 +6,11 @@ public class CommandLineCalculator5 {
     public static void main(String args[]) {
 
         String plus = "+", minus = "-", divide = "/", multiply = "*";
+        Boolean searchPlus, searchMinus;
+        Integer PointOfSign;
+        String num1, num2;
+        Double DoubleNum1, DoubleNum2, Result;
+
 
         Scanner scanline = new Scanner(System.in);
 
@@ -14,8 +19,23 @@ public class CommandLineCalculator5 {
         String expression = scanline.next();
 
 
-        System.out.println(expression.indexOf(plus));
-        System.out.println(expression.indexOf(minus));
+        searchPlus = expression.contains(plus);
+        searchMinus = expression.contains(minus);
+
+        if(searchPlus) {
+            PointOfSign = expression.indexOf(plus);
+            //System.out.println(PointOfSign);
+            num1 = expression.substring(0, PointOfSign);
+            num2 = expression.substring(PointOfSign+1, expression.length());
+            DoubleNum1 = Double.parseDouble(num1);
+            DoubleNum2 = Double.parseDouble(num2);
+            Result = DoubleNum1 + DoubleNum2;
+            System.out.println(num1 + " + " + num2 + " = " + Result);
+
+        }
+        else if(searchMinus) {
+            System.out.println(expression.indexOf(minus));
+        }
 
     }
 }
